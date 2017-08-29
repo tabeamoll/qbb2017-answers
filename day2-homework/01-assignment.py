4ce7cfd0c9e80ccbbc892ce58a2013e0d8e34222
Tabea Moll <tmoll@jhu.edu>
2017-08-29 17:00:29 -0400

    comments added to code

--- day2-homework/01-assignment.py
+++ day2-homework/01-assignment.py
@@ -2,16 +2,16 @@
 
 import sys
 
-f = open(sys.argv[1])
+f = open(sys.argv[1]). 
 
+#filter out all the lines that do not contain DROME
 for line in f:
-#    line = line.rstrip("\r\n")
     
     if "DROME" not in line:
         continue
-        
+#split those lines        
     else:
         row = line.split()
-
+#get only the last and the second to last column, separated by a tab
         print row[-1], "\t", row[-2]
         
