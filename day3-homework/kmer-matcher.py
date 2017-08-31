1d1b172a92cd38c1fb52d6b6765d26201fffb9e3
Tabea Moll <tmoll@jhu.edu>
2017-08-31 09:06:07 -0400

    better comments and fixed the tabs in the end

--- day3-homework/kmer-matcher.py
+++ day3-homework/kmer-matcher.py
@@ -2,13 +2,10 @@
 
 
 """
-things we need to do: 
-    1) strip the query of its lines and remove the header
-    2) kmer input on terminal
-    3) creat dic on query -> index for searching
-    
-        kmer_matcher.py <target.fa> <query.fa> <k>
-    *) output target_sequence_name    target_start    query_start   k-mer
+create a dictionary on the target sequence, including the kmer and their locations
+then go through the query and get its kmers and compare those to the dictionary of the target
+    input    kmer_matcher.py <target.fa> <query.fa> <k>
+    output   target_sequence_name    target_start    query_start   k-mer
 """
 
 import sys
@@ -44,7 +41,7 @@ for j in range(0, len(sequence_q)-k):
     if kmer_query in kmer_dict:
         result=kmer_dict[kmer_query]
         for item in result:
-            print item[0], "\t", item[1], "\t", j, "\t", kmer_query
+            print item[0] + "\t" + item[1] + "\t" + j + "\t", kmer_query
         
         
         
